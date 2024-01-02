e4bd83f043540ce685d9d727932d77c37a9636b1
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-02 15:20:56 +0100

    Refactor InputHandler Class
    The changes in the commit include a refactor of the InputHandler class. The class name has been capitalized and unnecessary spaces in string values have been removed.

--- input.js
+++ input.js
@@ -1,20 +1,21 @@
-export default class inputHandler {
+export default class InputHandler {
     constructor() {
         this.lastKey = '';
         window.addEventListener('keydown', (e) => {
             switch (e.key) {
                 case 'ArrowLeft':
-                    this.lastKey = 'PRESS  left';
+                    this.lastKey = 'PRESS left';
                     break;
                 case 'ArrowRight':
                     this.lastKey = 'PRESS right';
                     break;
             }
         });
+
         window.addEventListener('keyup', (e) => {
             switch (e.key) {
                 case 'ArrowLeft':
-                    this.lastKey = 'RELEASE  left';
+                    this.lastKey = 'RELEASE left';
                     break;
                 case 'ArrowRight':
                     this.lastKey = 'RELEASE right';
