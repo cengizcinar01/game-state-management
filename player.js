ded17ae0042587f5139e28efd8475b3cce8599cd
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-02 22:18:19 +0100

    Player Vertical Movement Addition
    The player's vertical movement and weight properties have been added in this update. This allows the player to move vertically and be affected by weight. A new method "onGround" has also been introduced to check if the player is on the ground.

--- player.js
+++ player.js
@@ -20,6 +20,8 @@ export default class Player {
         this.height = 181.83;
         this.x = this.gameWidth / 2 - this.width / 2;
         this.y = this.gameHeight - this.height;
+        this.vy = 0;
+        this.weight = 1;
         this.frameX = 0;
         this.frameY = 0;
         this.speed = 0;
@@ -34,9 +36,19 @@ export default class Player {
         this.x += this.speed;
         if (this.x <= 0) this.x = 0;
         else if (this.x >= this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+        // vertical movement
+        this.y += this.vy;
+        if (!this.onGround()) {
+            this.vy += this.weight;
+        } else {
+            this.vy = 0;
+        }
     }
     setState(state) {
         this.currentState = this.states[state];
         this.currentState.enter();
+    }
+    onGround() {
+        return this.y >= this.gameHeight - this.height;
     }
 }
